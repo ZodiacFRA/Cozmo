4a8ed4708a47a674d5b9dfe74f66596a69288ade
jb <jean-baptiste.dupuy@epitech.eu>
2019-10-30 15:58:45 +0000

    Clean up

--- Robot.py
+++ Robot.py
@@ -46,20 +46,6 @@ class Robot(object):
             print("Couldn't find a face.")
         return face
 
-    def move_forklit(self, speed=3):
-        """Positive speed goes up"""
-        self.robot.move_lift(speed)
-
-    def turn(self, angle=90):
-        """Positive angle goes left"""
-        self.robot.turn_in_place(degrees(angle)).wait_for_completed()
-
-    def move(self, distance=100, speed=150):
-        self.robot.drive_straight(distance_mm(distance), speed_mmps(speed)).wait_for_completed()
-
-    def speak(self, msg="I don't know what to say"):
-        self.robot.say_text(msg).wait_for_completed()
-
     def set_to_seek_position(self):
         """Move lift down, tilt head up, turn around to find something"""
         action1 = self.robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE / 2)
