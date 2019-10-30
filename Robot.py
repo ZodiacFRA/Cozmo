2954e7ed1235c04dbea0c6c3712100cdd9c3e018
jb <jean-baptiste.dupuy@epitech.eu>
2019-10-30 15:45:12 +0000

    Added seek player and found animation

--- Robot.py
+++ Robot.py
@@ -10,27 +10,26 @@ class Robot(object):
         self.robot = robot
         self.cube_target = None
         self.face_target = None
-        self.current_behavior = None
         self.cards = []
 
     def __del__(self):
         time.sleep(2)  # Wait for tasks completion before exiting
 
     def launch(self):
-		# Test only ATM
+        # Test only ATM
         self.set_to_seek_position()
-        self.seek_faces(15)
+        self.seek_player(15)
 
-    def seek_faces(self, timeout):
+    def seek_player(self, timeout):
         start_time = time.time()
         face = None
         while ((time.time() - start_time) < timeout) and not face:
                 self.robot.turn_in_place(degrees(36))
-                face = self.find_face_in_fov(2)
+                face = self.find_face_in_fov(2)  # Check for faces during 2 secs
         if face and face.is_visible:
-            self.robot.set_all_backpack_lights(cozmo.lights.blue_light)
-        else:
-            self.robot.set_backpack_lights_off()
+            robot.turn_towards_face(face).wait_for_completed()
+            self.robot.play_anim_trigger(cozmo.anim.Triggers.AcknowledgeFaceNamed).wait_for_completed()
+            self.face_target = face
 
     def find_face_in_fov(self, timeout=30):
         face = None
@@ -56,7 +55,7 @@ class Robot(object):
 
     def set_to_seek_position(self):
         """Move lift down, tilt head up, turn around to find something"""
-        action1 = self.robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE)
+        action1 = self.robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE / 2)
         action2 = self.robot.set_lift_height(0, in_parallel=True)
         action1.wait_for_completed()
         action2.wait_for_completed()
