2ee969895fadb2dd23beef66030015d4998cb678
jb <jean-baptiste.dupuy@epitech.eu>
2019-11-16 18:24:52 +0000

    Update add/remove markers

--- Robot.py
+++ Robot.py
@@ -2,57 +2,38 @@ import asyncio
 import time
 
 import cozmo
-from cozmo.util import degrees, distance_mm, speed_mmps
+from cozmo.objects import CustomObject, CustomObjectMarkers, CustomObjectTypes
 
 
-class Robot(object):
+class r(object):
     def __init__(self, robot):
-        self.robot = robot
-        self.cube_target = None
-        self.face_target = None
+        self.r = robot
         self.cards = []
 
     def __del__(self):
-        time.sleep(2)  # Wait for tasks completion before exiting
+        """Wait for tasks completion before exiting (needed by the Cozmo SDK)"""
+        time.sleep(2)
+
+    def handle_object_appeared(self, evt, **kw):
+        if isinstance(evt.obj, CustomObject):
+            print(f"{str(evt.obj.object_type)} appears")
+            if self.cards[-1] != evt.obj.object_type:
+                self.cards.append(evt.obj.object_type)
+
+    def handle_object_disappeared(self, evt, **kw):
+        if isinstance(evt.obj, CustomObject):
+            print(f"{str(evt.obj.object_type)} disappear")
+
+    def setup(self):
+        self.r.add_event_handler(cozmo.objects.EvtObjectAppeared, self.handle_object_appeared)
+        self.r.add_event_handler(cozmo.objects.EvtObjectDisappeared, self.handle_object_disappeared)
+        for action in ACTIONS:
+            if not self.r.world.define_custom_wall(CustomObjectTypes.CustomType00,
+                                    CustomObjectMarkers.Circles3, 297, 210, MARKERS_SIZE, True):
+                print(f"Marker {action} definition failed!")
+
 
     def launch(self):
-        self.set_to_seek_position()
-        while 42:
-            self.seek_player(timeout=15)
-            if not self.face_target:
-                print("Could not find a player")
-                self.robot.play_anim_trigger(cozmo.anim.Triggers.NothingToDoBoredIdle).wait_for_completed()
-            else:
-                self.robot.say_text("Let's play a game!").wait_for_completed()
-                self.robot.say_text("Show me control cards and I'll execute their actions in order").wait_for_completed()
-                self.robot.say_text("Try to make me stack a cube on top on another one").wait_for_completed()
-                self.robot.say_text("Let's go! Show me the actions you want me to execute!").wait_for_completed()
-                break
-
-    def seek_player(self, timeout):
-        start_time = time.time()
-        while ((time.time() - start_time) < timeout) and not self.face_target:
-            if self.face_target and self.face_target.is_visible:
-                self.robot.turn_towards_face(self.face_target).wait_for_completed()
-                break
-            else:
-                tmp = self.robot.turn_in_place(degrees(36))
-                self.face_target = self.find_face_in_fov(2)  # Check for faces during 2 secs
-            tmp.wait_for_completed()
-
-    def find_face_in_fov(self, timeout=30):
-        face = None
-        try:
-            face = self.robot.world.wait_for_observed_face(timeout)
-        except asyncio.TimeoutError:
-            print("Couldn't find a face.")
-        else:
-            self.robot.play_anim_trigger(cozmo.anim.Triggers.AcknowledgeFaceNamed).wait_for_completed()
-        return face
-
-    def set_to_seek_position(self):
-        """Move lift down, tilt head up, turn around to find something"""
-        action1 = self.robot.set_head_angle(cozmo.robot.MAX_HEAD_ANGLE / 2)
-        action2 = self.robot.set_lift_height(0, in_parallel=True)
-        action1.wait_for_completed()
-        action2.wait_for_completed()
+        print("Press CTRL-C to quit")
+        while True:
+            time.sleep(0.1)
---
+++ config.py
@@ -0,0 +1,12 @@
+MARKERS_SIZE = 180
+
+ACTIONS = [
+    "detect_cube",
+    "approach_cube",
+    "raise_fork",
+    "lower_fork",
+    "turn_left",
+    "turn_right",
+    "move_fwd",
+    "move_bckwd"
+]
